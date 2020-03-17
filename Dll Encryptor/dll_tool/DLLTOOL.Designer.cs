1acb902b6eef1f791765dcc437469ecfdb57847c
playday3008 <titanv3585@gmail.com>
2020-03-17 20:13:37 +0100

    Add error message if invalid path

--- Dll Encryptor/dll_tool/DLLTOOL.Designer.cs
+++ Dll Encryptor/dll_tool/DLLTOOL.Designer.cs
@@ -32,12 +32,14 @@
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.button1 = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(11, 54);
-			this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.textBox1.Location = new System.Drawing.Point(10, 50);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(2);
 			this.textBox1.Name = "textBox1";
 			this.textBox1.Size = new System.Drawing.Size(240, 20);
 			this.textBox1.TabIndex = 0;
@@ -45,8 +47,8 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(11, 78);
-			this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.button1.Location = new System.Drawing.Point(10, 100);
+			this.button1.Margin = new System.Windows.Forms.Padding(2);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(240, 25);
 			this.button1.TabIndex = 1;
@@ -64,16 +66,34 @@
 			this.label1.Text = "How do encrypt dll:\r\n1. Click on dll (Shift + RMouse) then \"Copy to path\"\r\n2. Pas" +
     "te here and delete quotes";
 			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(49, 75);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(201, 20);
+			this.textBox2.TabIndex = 3;
+			this.textBox2.Text = "XSaBw6JkWFZh7PBgLQb5TndqzEkm";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(8, 78);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Key:";
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(262, 114);
+			this.ClientSize = new System.Drawing.Size(262, 131);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.textBox1);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Margin = new System.Windows.Forms.Padding(2);
 			this.Name = "Form1";
 			this.Text = "DLL Encryptor";
 			this.ResumeLayout(false);
@@ -86,6 +106,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-    }
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+	}
 }
 
